d8e82af2cb42727d815c1c1256cb4e71b851e4e6
Vitaly Kramskikh <vkramskikh@gmail.com>
2012-05-21 18:19:03 +0400

    new items highlighting and sound notifications

--- lenta.py
+++ lenta.py
@@ -13,7 +13,7 @@ class LentaClient(object):
     def __init__(self):
         self.browser = mechanize.Browser()
         self.logged_in = False
-        
+
     def login(self, login, password):
         self.browser.open('http://id.lenta.ru/login/')
         self.browser.select_form(name='login')
@@ -22,19 +22,19 @@ class LentaClient(object):
         self.browser.submit()
         if self.browser.geturl() == 'http://id.lenta.ru/':
             self.logged_in = True
-    
+
     def comment(self, news_id, text, parent_id=None):
         self.browser.open('http://readers.lenta.ru/news/%s/' % news_id)
         self.browser.select_form(predicate=lambda form: form.attrs.get('id', '') == 'reply_form')
         self.browser.form.action = 'http://readers.lenta.ru/post_comment/'
-        
+
         self.browser['text'] = text
         if parent_id is not None:
             self.browser.form.find_control('parent_id').readonly = False
             self.browser['parent_id'] = str(parent_id)
-        
+
         response = self.browser.submit()
-        
+
         result = json.loads(response.read(), encoding='windows-1251')
         return result
 
@@ -44,19 +44,23 @@ class LentaClient(object):
 def index():
     return static_file('index.html', root='')
 
+@route('/static/:file')
+def static(file):
+    return static_file(file, root='')
+
 @route('/news/')
 def news():
     feed = feedparser.parse('http://lenta.ru/rss/')
     items = feed['items']
     items.sort(key=lambda item: item['published_parsed'])
-    
+
     output = map(lambda item: {
         'title': item['title'],
         'url': item['link'],
         'summary': item['summary'],
         'time': time.strftime('%H:%M', item['published_parsed']),
     }, items)
-    
+
     return json.dumps(output)
 
 @post('/comment/')
@@ -78,5 +82,5 @@ if __name__ == "__main__":
             clients.append(client)
         else:
             print "User %s is no longer valid" % user[0]
-    
+
     run(host='localhost', port=8080)
